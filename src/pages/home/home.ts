d62de0b301829a71750d1958a3593d84ea0bb423
faisal78676 <faisal78676@gmail.com>
2018-03-19 19:17:29 +0530

    1st commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,53 @@
+
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, ActionSheetController } from 'ionic-angular';
+import { MusicsProvider } from './../../providers/musics/musics';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+public allMusic:any = [];
+  constructor(public navCtrl: NavController,
+    private musicProvider:MusicsProvider,
+    private loadingController: LoadingController,
+    private actionSheet:ActionSheetController) {
+    
   }
+  public shareSong(){
+    let shareSongActionSheet = this.actionSheet.create({
+      title:'Share Song',
+      buttons:[
+        {
+          text:'Facebook',
+          icon:'logo-facebook'
+        },
+        {
+          text:'Twitter', 
+          icon:'logo-twitter'
+        },{
+          text:'Share',
+          icon:'share'
+        },
+        {
+          text:'Cancel',
+          role:'distructive'
+        }
+      ]
+    });
+    shareSongActionSheet.present();
+  }
+ionViewDidLoad(){
+  let allMusicLoadingController = this.loadingController.create({
+    content:'Getting Your Songs From Server'
+  });
+  allMusicLoadingController.present();
+  this.musicProvider.getMusic()
+  .subscribe((musicList) =>{ 
+    allMusicLoadingController.dismiss();
+    this.allMusic = musicList;
+  });
+}
 
 }
